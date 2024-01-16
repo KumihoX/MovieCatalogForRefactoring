effdda8f4dc443024dba31628488fe56e4bff980
KumihoX <v.dombrovskaya_1998@mail.ru>
2024-01-16 18:18:06 +0700

    Рефакторинг длинного метода
    - Выбранный метод рефакторинга: "Compose Method"
    
    - Обоснование выбранного метода:
    Так как было необходимо улучшить читаемость кода, то лучшим решением было использовать Compose Method, поскольку дробление кода на кусочки позволяет лучше понимать, за что эти кусочки ответственны.
    
    - Что было до рефакторинга:
    Код внутри функции ReviewCard был очень длинным(200+ строк) и не содержал в себе ни одного комментария.
    
    - Что стало после рефакторинга:
    Код функции ReviewCard сокращен до 100 строк, добавлены комментарии к кусочкам кода для лучшего восприятия, а также UI-элементы, объединенные какой-либо логикой были вынесены в отдельные функции и названы в соответствии с тем, что они из себя представляют.
    
    - Вывод:
    Улучшена читаемость кода, поскольку названия функций с UI-элементами теперь описывают их смысл(функционал).

--- app/src/main/java/com/example/emptycomposeactivity/screens/movieScreen/ReviewCard.kt
+++ app/src/main/java/com/example/emptycomposeactivity/screens/movieScreen/ReviewCard.kt
@@ -28,8 +28,6 @@ import com.example.emptycomposeactivity.R
 import com.example.emptycomposeactivity.screens.movieScreen.MovieViewModel.MovieScreenState
 import com.example.emptycomposeactivity.ui.theme.*
 
-
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ReviewCard(
     viewModel: MovieViewModel,
@@ -41,20 +39,9 @@ fun ReviewCard(
     rating: String,
     isUser: Boolean
 ) {
-
-    var avatar = image
-
-    if (avatar == "") {
-        avatar =
-            "https://s3-alpha-sig.figma.com/img/a92b/ba97/a13937d71ea4ab29b068a92fd325aa74?Expires=1668988800&Signature=PMOWOcFshz~iLSRZLvi6WE~CKCngLi7WpMoR44oMZEIROtNCJjaX3UxugCdLhzxYOX~iFsQ2YEt9GAe0yCLP0l8F4W7V-Ndc-3NFxenpkVmji5IweylmDYJ7ratNHIZ6NroftMSLiaPlglIssrOl0tg0NR~xPjrWKQLqOXLp9wFuoSvIm7IAcB4vNapJnOhMRF1Q9u1Da1h5H3Cl79Btg4WaB09aF7Yrf0IonCKszUYr189k6N7nDuQ5UgL7H9VeVtzkTNu1Y0SnjWYHONqOWHe8Q~3m3jo8eoAkX2OGYpm-QWKbJFGnqCbZkZoA~w3L8WVmSI4a6OHj8k-i16OStA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-    }
-
-    val color = ColorUtils.blendARGB(Red.toArgb(), Green.toArgb(), rating.toInt() * 0.1f)
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
-
             .padding(16.dp, 4.dp),
         backgroundColor = Black,
         border = BorderStroke(1.dp, White),
@@ -65,81 +52,21 @@ fun ReviewCard(
                 .padding(8.dp, 8.dp)
                 .fillMaxSize()
         ) {
+            // Шапка отзыва с информацией о пользователе и его оценкой фильма
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Row()
-                {
-                    Box(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .clip(CircleShape)
-                            .background(White)
-                    ) {
-                        Image(
-                            painter = rememberAsyncImagePainter(avatar),
-                            contentDescription = stringResource(R.string.user_avatar),
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            contentScale = ContentScale.Crop,
-                            alignment = Alignment.Center
-                        )
-                    }
-
-                    Column()
-                    {
-                        Text(
-                            text = nickName,
-                            modifier = Modifier
-                                .padding(8.dp, 0.dp, 0.dp, 0.dp),
-                            //.align(CenterVertically),
-                            color = White,
-                            fontSize = 16.sp,
-                            fontFamily = IBM,
-                            fontWeight = FontWeight.Medium,
-                        )
-                        if (isUser) {
-                            Text(
-                                text = "Мой отзыв",
-                                modifier = Modifier
-                                    .padding(8.dp, 0.dp, 0.dp, 0.dp),
-                                //.align(CenterVertically),
-                                color = Gray,
-                                fontSize = 12.sp,
-                                fontFamily = IBM,
-                                fontWeight = FontWeight.Medium,
-                            )
-                        }
-                    }
+                Row {
+                    UserAvatar(image = image)
+                    UserName(nickName = nickName, isUser = isUser)
                 }
-
-
-                Button(
-                    contentPadding = PaddingValues(16.dp, 4.dp),
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .defaultMinSize(minWidth = 42.dp, minHeight = 28.dp),
-                    onClick = {},
-                    enabled = false,
-                    shape = RoundedCornerShape(16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        disabledBackgroundColor = Color(color),
-                        disabledContentColor = White
-                    )
-                ) {
-                    Text(
-                        text = rating,
-                        color = White,
-                        fontSize = 16.sp,
-                        fontFamily = IBM,
-                        fontWeight = FontWeight.Medium,
-                    )
-                }
+                FilmRatingElement(rating)
             }
 
+            // Текст отзыва
             Text(
                 text = description,
                 modifier = Modifier.padding(0.dp, 8.dp, 0.dp, 4.dp),
@@ -157,6 +84,7 @@ fun ReviewCard(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Bottom
             ) {
+                // Дата создания отзыва
                 Text(
                     text = data,
                     color = Gray,
@@ -167,7 +95,7 @@ fun ReviewCard(
                 )
 
                 if (isUser) {
-
+                    // Окно для изменения отзыва
                     reviewDialog(
                         viewModel = viewModel,
                         state = state,
@@ -175,37 +103,123 @@ fun ReviewCard(
                         { viewModel.changeAnon(it) },
                         edit = true
                     )
-                    Row(
-                        modifier = Modifier
-                            .wrapContentWidth()
-                            .wrapContentHeight()
-                    )
-                    {
-                        CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false)
-                        {
-                            IconButton(onClick = { viewModel.interactionWithReviewDialog(true) }) {
-                                Icon(
-                                    modifier = Modifier
-                                        .padding(0.dp, 0.dp, 8.dp, 0.dp),
-                                    painter = painterResource(R.drawable.edit_icon),
-                                    contentDescription = stringResource(R.string.icon_edit),
-                                    tint = Color.Unspecified
-                                )
-                            }
-                            IconButton(onClick = { viewModel.deleteReview() }) {
-                                Icon(
-                                    modifier = Modifier
-                                        .padding(0.dp, 0.dp, 1.dp, 0.dp),
-                                    painter = painterResource(R.drawable.delete_icon),
-                                    contentDescription = stringResource(R.string.icon_delete),
-                                    tint = Color.Unspecified
-                                )
-                            }
-                        }
-                    }
+
+                    // Кнопки удаления и редактирования отзыва
+                    RowWithReviewActionsButtons(viewModel = viewModel)
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun RowWithReviewActionsButtons(viewModel: MovieViewModel) {
+    Row(
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false)
+        {
+            IconButton(onClick = { viewModel.interactionWithReviewDialog(true) }) {
+                Icon(
+                    modifier = Modifier
+                        .padding(0.dp, 0.dp, 8.dp, 0.dp),
+                    painter = painterResource(R.drawable.edit_icon),
+                    contentDescription = stringResource(R.string.icon_edit),
+                    tint = Color.Unspecified
+                )
+            }
+            IconButton(onClick = { viewModel.deleteReview() }) {
+                Icon(
+                    modifier = Modifier
+                        .padding(0.dp, 0.dp, 1.dp, 0.dp),
+                    painter = painterResource(R.drawable.delete_icon),
+                    contentDescription = stringResource(R.string.icon_delete),
+                    tint = Color.Unspecified
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserAvatar(image: String) {
+    var avatar = image
+
+    if (avatar == "") {
+        avatar =
+            "https://s3-alpha-sig.figma.com/img/a92b/ba97/a13937d71ea4ab29b068a92fd325aa74?Expires=1668988800&Signature=PMOWOcFshz~iLSRZLvi6WE~CKCngLi7WpMoR44oMZEIROtNCJjaX3UxugCdLhzxYOX~iFsQ2YEt9GAe0yCLP0l8F4W7V-Ndc-3NFxenpkVmji5IweylmDYJ7ratNHIZ6NroftMSLiaPlglIssrOl0tg0NR~xPjrWKQLqOXLp9wFuoSvIm7IAcB4vNapJnOhMRF1Q9u1Da1h5H3Cl79Btg4WaB09aF7Yrf0IonCKszUYr189k6N7nDuQ5UgL7H9VeVtzkTNu1Y0SnjWYHONqOWHe8Q~3m3jo8eoAkX2OGYpm-QWKbJFGnqCbZkZoA~w3L8WVmSI4a6OHj8k-i16OStA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+    }
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(White)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(avatar),
+            contentDescription = stringResource(R.string.user_avatar),
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center
+        )
+    }
+}
+
+@Composable
+private fun UserName(
+    nickName: String,
+    isUser: Boolean
+) {
+    Column {
+        Text(
+            text = nickName,
+            modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
+            color = White,
+            fontSize = 16.sp,
+            fontFamily = IBM,
+            fontWeight = FontWeight.Medium,
+        )
+        if (isUser) {
+            Text(
+                text = "Мой отзыв",
+                modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
+                color = Gray,
+                fontSize = 12.sp,
+                fontFamily = IBM,
+                fontWeight = FontWeight.Medium,
+            )
+        }
+    }
+}
+
+@Composable
+private fun FilmRatingElement(rating: String) {
+    val color = ColorUtils.blendARGB(Red.toArgb(), Green.toArgb(), rating.toInt() * 0.1f)
+
+    Button(
+        contentPadding = PaddingValues(16.dp, 4.dp),
+        modifier = Modifier
+            .wrapContentSize()
+            .defaultMinSize(minWidth = 42.dp, minHeight = 28.dp),
+        onClick = {},
+        enabled = false,
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            disabledBackgroundColor = Color(color),
+            disabledContentColor = White
+        )
+    ) {
+        Text(
+            text = rating,
+            color = White,
+            fontSize = 16.sp,
+            fontFamily = IBM,
+            fontWeight = FontWeight.Medium,
+        )
     }
 }
 
